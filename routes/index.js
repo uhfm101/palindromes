a2f2be2edb42bc06341f109436663b0db2c01088
uhfm <uhfm@fresnou.org>
2022-09-30 11:33:25 -0700

    commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    phrase: ""
+  });
+});
+
+router.post('/', function(req, res){
+  res.render('index', {
+    phrase: req.body.userText,
+    message: getResult(req.body.userText)
+  })
+})
+
+module.exports = router;
+
+function pChecker(phrase) {
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("");
+  if (phrase.toLowerCase().replace(" ", "") === temp.toLowerCase().replace(" ", "")) {
+    return true;
+} else{
+    return false;
+  }
+}
+
+function getResult(phrase){
+  if (pChecker(phrase)){
+    return `${phrase} is a palindrome`
+  }
+  return `${phrase} is not a palindrome`
+}
+
